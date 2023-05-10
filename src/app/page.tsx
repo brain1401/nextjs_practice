e49bf2c6f49dcfb60370eda7721450fc8cb57e42
Aiden Hong <brain1401@gmail.com>
2023-05-10 21:34:00 +0900

    서버 컴포넌트 연습

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import os from 'os' // 노드APIs
 
 export default function Home() {
+
+  console.log(os.hostname()); //서버 컴포넌트라서 노드API 사용 가능
   return (
     <div>
       <h1>홈페이지다</h1>
