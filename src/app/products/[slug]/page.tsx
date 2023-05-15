8ac54f7172671b3f0b819bffe9944d2e54273e6c
Aiden Hong <brain1401@gmail.com>
2023-05-15 21:57:04 +0900

    SSG 렌더링 연습

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,24 +1,44 @@
+import NotFoundPage from "@/app/not-found";
+import { getProduct, getProducts } from "@/service/products";
+import { Metadata } from "next";
+
 type Props = {
   params: {
     slug: string;
   };
 };
 
-export function generateMetadata({ params }: Props) {
+export async function generateMetadata({ params: {slug} }: Props):Promise<Metadata> {
+  
+  const product = await getProduct(slug);
+
   return {
-    title: `제품의 이름 : ${params.slug}`,
+    title: `제품의 이름 : ${product?.name}`,
   }
 }
 
-export default function PantsPage({params}: Props) {
-  return <h1>{params.slug} 제품 설명 페이지</h1>;
+export default async function ProductPage({params : {slug}}: Props) {
+  
+  const product = await getProduct(slug);
+
+  if (product === undefined) {
+    NotFoundPage();
+  }
+  else{
+
+    return <h1>{product.name} 제품 설명 페이지</h1>;
+  }
+
+  
 }
 
 
-export function generateStaticParams() {
-  const products = ['pants', 'skirt'];
+export async function generateStaticParams() {
+  //모든 제품의 페이지들을 미리 만들어 둘 수 있게 함 (SSG)
+  
+  const products = await getProducts();
 
   return products.map(product => ({
-    slug: product,
+    slug: product.id,
   }));
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
 import styles from './page.module.css'
-export default function ProductsPage() {
+import { Product, getProducts } from "@/service/products";
 
-  const products: string[] = ['pants','skirt','shirt','shoes'];
+export default async function ProductsPage() {
+
+  const products = await getProducts();
 
   return (
     <div className={styles.mainbody}>
       <h1>제품 소개 페이지!</h1>
 
       <ul>
-        {products.map((item: string, i: number) => (
-          <li key={i} className={styles.product}>
-            <Link href={`/products/${item}`}>{item}</Link>
+        {products.map((product: Product, i: number) => (
+          <li key={product.id} className={styles.product}>
+            <Link href={`/products/${product.id}`}>{product.name}</Link>
           </li>
         ))}
       </ul>
