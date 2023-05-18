fae3eff5f6c51afb683f653b36e04254fe4aa731
Aiden Hong <brain1401@gmail.com>
2023-05-18 16:16:23 +0900

    Products페이지 SSG/CSR 동시구현

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import styles from './page.module.css'
 import { Product, getProducts } from "@/service/products";
+import MeowArticle from "@/components/MoewArticle";
 
-export const revalidate = 3; //3초마다 ISR을 진행
+// export const revalidate = 3; //3초마다 ISR을 진행
 
 export default async function ProductsPage() {
 
   const products = await getProducts();
-
   return (
     <div className={styles.mainbody}>
       <h1>제품 소개 페이지!</h1>
@@ -19,6 +19,7 @@ export default async function ProductsPage() {
           </li>
         ))}
       </ul>
+      <MeowArticle/>
     </div>
   );
 }
