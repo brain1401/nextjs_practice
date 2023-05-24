d6cf6453c16b44fb8a5687842841eb2c36fce730
Aiden Hong <brain1401@gmail.com>
2023-05-25 00:13:41 +0900

    products/page.tsx 로직 보완

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -6,23 +6,26 @@ import MeowArticle from "@/components/MeowArticle";
 import clotheImage from '../../../public/images/clothes.jpg'
 
 export default async function ProductsPage() {
+  const ognlProducts:Product[] = await getProducts();
 
-  const products = await getProducts();
-
+  const idsToRemove = ["man", "women"];
+  const processedProducts = ognlProducts.filter(
+    (product: Product) => !idsToRemove.includes(product.id)
+  );
 
   return (
     <div className={styles.mainbody}>
       <h1>제품 소개 페이지!</h1>
 
-      <Image src={clotheImage} alt="Clothes"/>
+      <Image src={clotheImage} alt="Clothes" />
       <ul>
-        {products.map((product: Product, i: number) => (
+        {processedProducts.map((product: Product, i: number) => (
           <li key={product.id} className={styles.product}>
             <Link href={`/products/${product.id}`}>{product.name}</Link>
           </li>
         ))}
       </ul>
-      <MeowArticle/>
+      <MeowArticle />
     </div>
   );
 }
