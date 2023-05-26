db34ceaf71f1525a13bfdadea35bc1ebef527245
Aiden Hong <brain1401@gmail.com>
2023-05-26 21:53:57 +0900

    redirect 연습

--- next.config.js
+++ next.config.js
@@ -10,6 +10,22 @@ const nextConfig = {
         hostname: 'images.unsplash.com',
       }
     ]
+  },
+
+  async redirects() { //redirect 설정 
+    return [
+      {
+        source: "/products/deleted_forever",
+        destination: "/products",
+        permanent: true, //이 permanet값에 따라서 Search Engine이 다르게 상호작용한다.
+      },
+      {
+        source: "/products/deleted_temp",
+        destination: "/products",
+        permanent: false,
+      },
+    ];
+
   }
 }
 
