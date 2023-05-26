7d185772e39e17411adf74699f9c363a8db72605
Aiden Hong <brain1401@gmail.com>
2023-05-27 03:28:53 +0900

    ver4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Counter from "@/components/Counter";
 export default function Home() {
   return (
     <>
-      <h1>홈페이지다!</h1>
+      <h1>홈페이지다! 버전4</h1>
       {/* <Counter /> */}
       <br />
       <Image
